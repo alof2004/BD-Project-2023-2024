e1bd3f58b44857e91529189bbb6b12cd63332de5
alof04 <hpclafonso@gmail.com>
2024-05-13 16:08:12 +0100

    .

---
+++ SQLQuery1 (2).sql
@@ -0,0 +1,259 @@
+--create DATABASE AgroTrack
+--go 
+IF OBJECT_ID ('AgroTrack_Compra','U') IS NOT NULL
+	DROP TABLE dbo.AgroTrack_Compra;
+IF OBJECT_ID ('AgroTrack_Item','U') IS NOT NULL
+	DROP TABLE dbo.AgroTrack_Item;
+IF OBJECT_ID ('AgroTrack_Encomenda','U') IS NOT NULL
+	DROP TABLE dbo.AgroTrack_Encomenda;
+IF OBJECT_ID ('AgroTrack_Colhe','U') IS NOT NULL
+	DROP TABLE dbo.AgroTrack_Colhe;
+IF OBJECT_ID ('AgroTrack_Contem','U') IS NOT NULL
+	DROP TABLE dbo.AgroTrack_Contem;
+IF OBJECT_ID ('AgroTrack_Empresa_De_Transportes','U') IS NOT NULL
+	DROP TABLE dbo.AgroTrack_Empresa_De_Transportes;
+IF OBJECT_ID ('AgroTrack_Retalhistas','U') IS NOT NULL
+	DROP TABLE dbo.AgroTrack_Retalhistas;
+IF OBJECT_ID ('AgroTrack_Contrato','U') IS NOT NULL
+	DROP TABLE dbo.AgroTrack_Contrato;
+	IF OBJECT_ID ('AgroTrack_Produto') IS NOT NULL
+	DROP TABLE dbo.AgroTrack_Produto;
+IF OBJECT_ID ('AgroTrack_Agricultor','U') IS NOT NULL
+	DROP TABLE dbo.AgroTrack_Agricultor;
+IF OBJECT_ID ('AgroTrack_Cliente','U') IS NOT NULL
+	DROP TABLE dbo.AgroTrack_Cliente;
+	IF OBJECT_ID ('AgroTrack_Quinta_Planta','U') IS NOT NULL
+	DROP TABLE dbo.AgroTrack_Quinta_Planta;
+IF OBJECT_ID ('AgroTrack_Planta','U') IS NOT NULL
+	DROP TABLE dbo.AgroTrack_Planta;
+IF OBJECT_ID ('AgroTrack_Quinta_Animal','U') IS NOT NULL
+	DROP TABLE dbo.AgroTrack_Quinta_Animal;
+IF OBJECT_ID ('AgroTrack_Animal','U') IS NOT NULL
+	DROP TABLE dbo.AgroTrack_Animal;
+IF OBJECT_ID ('AgroTrack_Quinta','U') IS NOT NULL
+	DROP TABLE dbo.AgroTrack_Quinta;
+IF OBJECT_ID ('AgroTrack_Empresa','U') IS NOT NULL
+	DROP TABLE dbo.AgroTrack_Empresa;
+IF OBJECT_ID ('AgroTrack_Pessoa','U') IS NOT NULL
+	DROP TABLE dbo.AgroTrack_Pessoa;
+
+
+
+
+create table AgroTrack_Pessoa (
+	Nome				varchar(64)			not null,
+	N_CartaoCidadao		int					not null		check(len(N_CartaoCidadao)=8),
+	Contacto			int					not null		check(len(Contacto)=9),
+
+	PRIMARY KEY(N_CartaoCidadao)
+);
+
+
+create table AgroTrack_Empresa (
+	Id_Empresa			int					not null,
+	Nome				varchar(64)			not null,
+    Morada				varchar(64)			not null,
+
+	PRIMARY KEY(Id_Empresa),
+	unique (Nome)
+);
+
+create table AgroTrack_Quinta(
+	Codigo_quinta		int					not null,
+	Empresa_Id_Empresa	int,
+
+	PRIMARY KEY(Empresa_Id_Empresa)
+);
+
+create table AgroTrack_Animal(
+	Id_Animal			int					not null,
+	Tipo_de_Animal		varchar(64)			not null,	
+
+	PRIMARY KEY (Id_Animal)
+
+);
+
+create table AgroTrack_Quinta_Animal(
+	Empresa_Id_Empresa	int,
+	Idade				int					not null,
+	Brinco			    varchar(16)		    not null,
+	Id_Animal			int					not null,
+
+	PRIMARY KEY (Id_Animal,Empresa_Id_Empresa,Brinco)
+
+);
+
+create table AgroTrack_Planta(
+	Id_planta			int					not null,
+	Tipo				varchar(32)			not null,
+    Estacao VARCHAR(32) NOT NULL CHECK (Estacao IN ('Primavera', 'Verão', 'Outono', 'Inverno', 'Todas')),
+
+	PRIMARY KEY (Id_planta)
+
+);
+create table AgroTrack_Quinta_Planta(
+	Empresa_Id_Empresa	int,
+	Lote				varchar(32)		    not null,
+	Id_Planta			int					not null,
+
+	PRIMARY KEY (Id_Planta,Empresa_Id_Empresa,Lote)
+
+);
+
+
+
+create table AgroTrack_Cliente (
+	Pessoa_N_CartaoCidadao int,
+	Quinta_Empresa_Id_Empresa int,
+
+	PRIMARY KEY(Pessoa_N_CartaoCidadao)
+
+);
+
+create table AgroTrack_Agricultor (
+	Id_Trabalhador			int			    not null,
+	Pessoa_N_CartaoCidadao	int,
+	Quinta_Empresa_Id_Empresa	int,
+
+	PRIMARY KEY(Pessoa_N_CartaoCidadao)
+);
+
+create table AgroTrack_Produto(
+	Nome				varchar(64)			not null,
+	Quantidade			int					not null,		
+	Id_origem			int					not null,		
+	Tipo_de_Produto		int					not null,
+	Codigo				int					not null, 
+	Preco				float               not null        check(Preco > 0),	
+	[Data_de_validade]	date				not null,
+	Taxa_de_iva			float				not null		check (Taxa_de_iva IN (0.06, 0.13, 0.23)),
+	Unidade_medida      int					not null        check (Unidade_medida IN ('unidade', 'kg', 'g', 'litro', 'ml')),
+	Agricultor_Pessoa_N_CartaoCidadao int,
+
+	PRIMARY KEY (Codigo)
+
+);
+create table AgroTrack_Contrato(
+	[Date_str]			date				not null,
+	[Date_end]			date				not null,
+	Descricao			varchar(64) 		not null,
+	Salario				float               not null        check(Salario > 0),
+	ID					int					not null, 
+	Agricultor_Pessoa_N_CartaoCidadao	int,
+	
+	CHECK (Date_end > [Date_str]),
+    PRIMARY KEY(ID),
+    UNIQUE ([Descricao])
+);
+
+
+
+create table AgroTrack_Retalhistas (
+	Empresa_Id_Empresa			int,
+
+
+	PRIMARY KEY(Empresa_Id_Empresa)
+);
+
+
+create table AgroTrack_Empresa_De_Transportes (
+	Empresa_Id_Empresa			int,
+
+	PRIMARY KEY(Empresa_Id_Empresa)
+);
+
+create table AgroTrack_Contem (
+	Produto_codigo			int,
+	Quinta_Empresa_Id_Empresa int,
+	Quantidade			int					not null,
+
+	PRIMARY KEY(Produto_codigo,Quinta_Empresa_Id_Empresa) 
+);
+
+create table AgroTrack_Colhe (
+	Empresa_Id_Empresa			int,
+	Agricultor_Pessoa_N_CartaoCidadao int, 
+	Duracao_colheita	float				not null,
+	Quantidade			int					not null,
+	Produto_codigo			int,
+
+
+	PRIMARY KEY(Agricultor_Pessoa_N_CartaoCidadao,Empresa_Id_Empresa)
+);
+
+
+create table AgroTrack_Encomenda(
+	Codigo				int					not null,
+	prazo_entrega		int					not null,
+	Morada_entrega		varchar(64)			not null, 
+	Entrega				date				not null,
+	Retalhista_Empresa_Id_Empresa	int,
+	Empresa_De_Transportes_Id_Empresa	int,
+	Quinta_Empresa_Id	int,
+
+
+	PRIMARY KEY (Codigo)
+);
+
+create table AgroTrack_Item(
+	Codigo				int					not null,
+	Quantidade			int					not null, 
+	Encomenda_Codigo	int,
+
+	PRIMARY KEY (Codigo,Encomenda_Codigo)
+
+);
+
+create table AgroTrack_Compra(
+	Produto_codigo			int,
+	Cliente_Pessoa_N_CartaoCidadao	int,
+	Preco				float               not null        check(Preco > 0),
+	Quantidade			int					not null, 
+	Metodo_de_pagamento varchar(64)			not null,
+
+	PRIMARY KEY (Produto_codigo,Cliente_Pessoa_N_CartaoCidadao)
+
+); 
+
+
+
+alter table AgroTrack_Quinta add constraint Empresa_Id_EmpresaPk_Q foreign key(Empresa_Id_Empresa) references AgroTrack_Empresa(Id_Empresa); 
+
+
+alter table AgroTrack_Cliente add constraint Pessoa_N_Cartao_CidadaoPK_C foreign key(Pessoa_N_CartaoCidadao) references AgroTrack_Pessoa(N_CartaoCidadao);
+alter table AgroTrack_Cliente add constraint Quinta_Empresa_Id_EmpresaFK_C foreign key(Quinta_Empresa_Id_Empresa) references AgroTrack_Quinta(Empresa_Id_Empresa);
+					 
+alter table AgroTrack_Retalhistas add constraint Empresa_Id_EmpresaPK_R foreign key(Empresa_Id_Empresa) references AgroTrack_Empresa(Id_Empresa);
+
+alter table AgroTrack_Agricultor add constraint Quinta_Empresa_Id_EmpresaFK_A foreign key(Quinta_Empresa_Id_Empresa) references AgroTrack_Quinta(Empresa_Id_Empresa); 
+
+alter table AgroTrack_Quinta_Animal add constraint Id_AnimalFK_AN foreign key(Id_Animal) references AgroTrack_Animal(Id_Animal); 
+alter table AgroTrack_Quinta_Animal add constraint Empresa_Id_EmpresaFK_AN foreign key(Empresa_Id_Empresa) references AgroTrack_Quinta(Empresa_Id_Empresa); 
+					 
+alter table AgroTrack_Quinta_Planta add constraint Id_PlantaFK_P foreign key(Id_Planta) references AgroTrack_Planta(Id_Planta); 
+alter table AgroTrack_Quinta_Planta add constraint Empresa_Id_EmpresaFK_P foreign key(Empresa_Id_Empresa) references AgroTrack_Quinta(Empresa_Id_Empresa); 
+					 
+alter table AgroTrack_Empresa_De_Transportes add constraint Empresa_Id_EmpresaFK_E foreign key(Empresa_Id_Empresa) references AgroTrack_Empresa(Id_Empresa);
+
+alter table AgroTrack_Retalhistas add constraint Empresa_Id_EmpresaFK foreign key(Empresa_Id_Empresa) references AgroTrack_Empresa(Id_Empresa); 
+					 
+alter table AgroTrack_Contem add constraint Produto_CodigoFK_CO foreign key(Produto_codigo) references AgroTrack_Produto(Codigo); 
+alter table AgroTrack_Contem add constraint Quinta_Empresa_Id_EmpresaFK_CO foreign key(Quinta_Empresa_Id_Empresa) references AgroTrack_Quinta(Empresa_Id_Empresa); 
+					 
+					 
+alter table AgroTrack_Colhe add constraint Agricultor_Pessoa_N_CartaoCidadaoFK_C foreign key(Agricultor_Pessoa_N_CartaoCidadao) references AgroTrack_Agricultor(Pessoa_N_CartaoCidadao); 
+alter table AgroTrack_Colhe add constraint Produto_codigoFK_C foreign key(Produto_codigo) references AgroTrack_Produto(Codigo); 
+					 
+alter table AgroTrack_Contrato add constraint Agricultor_Pessoa_N_CartaoCidadaoFK foreign key(Agricultor_Pessoa_N_CartaoCidadao) references AgroTrack_Agricultor(Pessoa_N_CartaoCidadao); 
+					 
+alter table AgroTrack_Item add constraint Encomenda_CodigoFK foreign key(Encomenda_Codigo) references AgroTrack_Encomenda(Codigo); 
+					 
+alter table AgroTrack_Encomenda add constraint Retalhista_Empresa_Id_EmpresaFK foreign key(Retalhista_Empresa_Id_Empresa) references AgroTrack_Retalhistas(Empresa_Id_Empresa); 
+alter table AgroTrack_Encomenda add constraint Empresa_De_Transportes_Id_EmpresaFK foreign key(Empresa_De_Transportes_Id_Empresa) references AgroTrack_Empresa_De_Transportes(Empresa_Id_Empresa); 
+alter table AgroTrack_Encomenda add constraint Quinta_Empresa_IdFK foreign key(Quinta_Empresa_Id) references AgroTrack_Quinta(Empresa_Id_Empresa);
+					
+alter table AgroTrack_Produto add constraint Agricultor_Pessoa_N_CartaoCidadaoFK_P foreign key(Agricultor_Pessoa_N_CartaoCidadao) references AgroTrack_Agricultor(Pessoa_N_CartaoCidadao); 
+					 
+alter table AgroTrack_Compra add constraint Produto_codigoFK_Compra foreign key(Produto_codigo) references AgroTrack_Produto(Codigo); 
+alter table AgroTrack_Compra add constraint Client_Pessoa_N_CartaoCidadaoFK_Compra foreign key(Cliente_Pessoa_N_CartaoCidadao) references AgroTrack_Cliente(Pessoa_N_CartaoCidadao); 
+					 
---
+++ insert.sql
@@ -0,0 +1,225 @@
+INSERT INTO AgroTrack_Pessoa(Nome,N_CartaoCidadao,Contacto) VALUES
+('João Silva', 12345678, 912345678),
+('Jorge Miguel', 87654321, 912345678),
+('Maria Sousa', 98765432, 934567890),
+('António Ferreira', 45678912, 923456789),
+('Afonso Lucas', 18041804, 918049638),
+('Beatriz Oliveira', 75395128, 925874136),
+('Pedro Santos', 36925814, 917253684),
+('Sofia Costa', 24681357, 926487135),
+('Miguel Rodrigues', 58246913, 913824769),
+('Carolina Pereira', 31472658, 919372645),
+('Rui Martins', 95135786, 928465713),
+('Inês Fernandes', 82461357, 927346185),
+('Luís Almeida', 75315986, 915278463),
+('Ana Sousa', 36985214, 931475869),
+('Tiago Oliveira', 74125896, 932587146),
+('Mariana Silva', 58214736, 916243785),
+('Francisco Pereira', 36914725, 938571624),
+('Catarina Santos', 15948762, 935824716),
+('Diana Costa', 78523691, 934185276),
+('Gonçalo Rodrigues', 25836947, 917524836),
+('Rita Martins', 36985247, 936487251),
+('Eduardo Oliveira', 78945612, 910203040),
+('Isabel Santos', 65498732, 915171819),
+('Leonardo Costa', 32165498, 920304050),
+('Marta Ferreira', 98732165, 925262728),
+('Ricardo Pereira', 45612378, 930384950),
+('Filipa Carvalho', 36985214, 931475869),
+('Hugo Rodrigues', 74125896, 932587146),
+('Vera Santos', 58214736, 916243785),
+('David Oliveira', 36914725, 938571624),
+('Andreia Costa', 15948762, 935824716),
+('Xavier Martins', 36985249, 936487253),
+('Yvone Oliveira', 78945614, 910203042),
+('Zé Santos', 65498734, 915171821),
+('Álvaro Costa', 32165400, 920304052),
+('Bárbara Ferreira', 98732167, 925262730),
+('Carlos Pereira', 45612380, 930384952),
+('Diana Carvalho', 36985217, 931475873),
+('Eduardo Rodrigues', 74125903, 932587150),
+('Fátima Santos', 58214740, 916243788),
+('Gilberto Oliveira', 36914728, 938571628),
+('Helena Costa', 15948766, 935824720);
+
+INSERT INTO AgroTrack_Empresa(Id_Empresa, Nome, Morada) VALUES
+(1, 'Quinta da Vinha Verde', 'Rua Principal, 123'),
+(2, 'Quinta da Esperança', 'Avenida Central, 456'),
+(3, 'Quinta da Boa Vista', 'Travessa Secundária, 789'),
+(4, 'Quinta da Fonte Fresca', 'Rua das Flores, 10'),
+(5, 'Quinta das Oliveiras', 'Avenida dos Aliados, 20'),
+(6, 'Quinta do Sol Nascente', 'Praça da Liberdade, 30'),
+(7, 'Quinta da Primavera', 'Rua dos Lírios, 15'),
+(8, 'Quinta do Sol Poente', 'Avenida das Oliveiras, 25'),
+(9, 'Logística Ágil', 'Avenida das Entregas Eficientes, 60'),
+(10, 'Fretes Seguros', 'Travessa dos Transportes Seguros, 70'),
+(11, 'Transportes Velozes LTDA', 'Rua das Rotas Rápidas, 80'),
+(12, 'Logística Global', 'Avenida da Eficiência Logística, 90'),
+(13, 'Entrega Segura', 'Travessa da Entrega Certa, 100'),
+(14, 'Transportadora Ágil Express', 'Rua da Entrega Expressa, 110'),
+(15, 'Fretes Rápidos SA', 'Avenida dos Fretes, 120'),
+(16, 'Logística Integrada', 'Travessa da Logística, 130'),
+(17, 'Supermercado Central', 'Rua Principal, 250'),
+(18, 'Loja do Bairro', 'Avenida Central, 260'),
+(19, 'Casa dos Utensílios', 'Travessa das Utilidades, 270'),
+(20, 'Continente', 'Rua das Novidades, 280'),
+(21, 'Armazém Geral', 'Avenida das Mercadorias, 290'),
+(22, 'Loja do Comércio', 'Travessa do Comércio, 300'),
+(23, 'Hipermercado Mega', 'Rua da Economia, 310'),
+(24, 'Pingo Doce', 'Avenida da Variedade, 320');
+
+INSERT INTO AgroTrack_Quinta(Codigo_quinta, Empresa_Id_Empresa) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 6),
+(7, 7),
+(8, 8);
+
+INSERT INTO AgroTrack_Animal(Id_Animal, Tipo_de_Animal) VALUES
+(1, 'Vaca'),
+(2, 'Ovelha'),
+(3, 'Porco'),
+(4, 'Frango'),
+(5, 'Cavalo'),
+(6, 'Cabra'),
+(7, 'Pato'),
+(8, 'Ganso'),
+(9, 'Gato'),
+(10, 'Coelho'),
+(11, 'Burro'),
+(12, 'Galinha'),
+(13, 'Peru'),
+(14, 'Cão');
+
+INSERT INTO AgroTrack_Quinta_Animal(Empresa_Id_Empresa, Idade, Brinco, Id_Animal) VALUES
+(1, 5, '1VAC', 1),
+(1, 8, '2VAC', 1),
+(1, 4, '1BUR', 11),
+(1, 6, '2BUR', 11),
+(2, 1, '1GLN', 12),
+(2, 2, '2GLN', 12),
+(2, 1, '1CÃO', 14),
+(2, 2, '2CÃO', 14),
+(2, 3, '1GAN', 8),
+(3, 3, '1PRU', 13),
+(3, 4, '2PRU', 13),
+(2, 5, '2GAN', 8),
+(3, 2, '3POR', 3),
+(3, 3, '4POR', 3),
+(4, 1, '1OVL', 2),
+(4, 2, '2OVL', 2),
+(5, 4, '1CAV', 5),
+(5, 6, '2CAV', 5),
+(6, 2, '1CAB', 6),
+(6, 3, '2CAB', 6),
+(7, 1, '1PAT', 7),
+(7, 2, '2PAT', 7),
+(8, 1, '1GAN', 8),
+(8, 2, '2GAN', 8);
+
+
+INSERT INTO AgroTrack_Planta(Id_planta, Tipo, Estacao) VALUES
+(1,'Macieira', 'Primavera'),
+(2,'Alface', 'Verão'),
+(3,'Macieira', 'Verão'),
+(4,'Tomate', 'Outono'),
+(5,'Laranjeira', 'Todas'),
+(6, 'Pereira', 'Primavera'),
+(7,'Alface', 'Primavera'),
+(8,'Tomate', 'Primavera'),
+(9,'Milho', 'Verão'),
+(10,'Cerejeira', 'Verão'),
+(11,'Melão', 'Verão'),
+(12,'Bananeira', 'Todas'),
+(14, 'Cenoura', 'Inverno'),
+(15, 'Abóbora', 'Outono'),
+(16, 'Morangueiro', 'Primavera'),
+(17, 'Videira', 'Verão'),
+(18, 'Feijão', 'Verão'),
+(19, 'Cebola', 'Inverno'),
+(20, 'Batata', 'Inverno');
+
+INSERT INTO AgroTrack_Quinta_Planta(Empresa_Id_Empresa, Lote, Id_Planta) VALUES
+(1, 'Lote_A', 1),   -- Macieira na Empresa 1
+(1, 'Lote_B', 5),   -- Laranjeira na Empresa 1
+(2, 'Lote_C', 2),   -- Alface na Empresa 2
+(2, 'Lote_D', 6),   -- Pereira na Empresa 2
+(3, 'Lote_E', 3),   -- Macieira na Empresa 3
+(3, 'Lote_F', 8),   -- Tomate na Empresa 3
+(4, 'Lote_G', 4),   -- Tomate na Empresa 4
+(4, 'Lote_H', 9),   -- Milho na Empresa 4
+(5, 'Lote_I', 7),   -- Alface na Empresa 5
+(5, 'Lote_J', 10),  -- Cerejeira na Empresa 5
+(6, 'Lote_K', 11),   -- Melão na Empresa 6
+(6, 'Lote_L', 12),   -- Bananeira na Empresa 6
+(7, 'Lote_M', 13),   -- Cenoura na Empresa 7
+(7, 'Lote_N', 14),   -- Cenoura na Empresa 7
+(8, 'Lote_O', 15),   -- Abóbora na Empresa 8
+(8, 'Lote_P', 16);   -- Morangueiro na Empresa 8
+
+
+INSERT INTO AgroTrack_Cliente(Pessoa_N_CartaoCidadao) VALUES --NAO SEI 
+
+INSERT INTO AgroTrack_Agricultor(Id_Trabalhador, Pessoa_N_CartaoCidadao,Quinta_Empresa_Id_Empresa) VALUES
+(1, 12345678, 1),
+(2, 87654321, 1),
+(1, 18041804, 2),
+(2, 15948762, 2),
+(1, 36985247, 3),
+(2, 95135786, 3),
+(1, 74125896, 4),
+(2, 36914725, 4),
+(1, 58214736, 5),
+(2, 36985214, 5),
+(1, 15948762, 6),
+(2, 36914725, 6),
+(1, 45612378, 7),
+(2, 58214740, 7),
+(1, 36914728, 8),
+(2, 15948766, 8);
+
+INSERT INTO AgroTrack_Produto(Nome, Quantidade, Id_origem, Tipo_de_Produto, Codigo, Preco, Data_de_validade, Taxa_de_iva, Unidade_medida, Agricultor_Pessoa_N_CartaoCidadao
+
+INSERT INTO AgroTrack_Contrato(
+    Date_str, 
+    Date_end, 
+    Descricao, 
+    Salario, 
+    ID, 
+    Agricultor_Pessoa_N_CartaoCidadao
+) SELECT
+    '2024-01-01', -- Data de início fixa
+    '2024-12-31', -- Data de fim fixa
+    'Contrato Padrão', -- Descrição do contrato
+    1000, -- Salário fixo
+    Id_Trabalhador, -- ID do agricultor
+    Pessoa_N_CartaoCidadao -- Número do cartão do cidadão do agricultor
+FROM AgroTrack_Agricultor;
+
+INSERT INTO AgroTrack_Retalhistas VALUES
+	(17),
+	(18),
+	(19),
+	(20),
+	(21),
+	(22),
+	(23),
+	(24);
+
+INSERT INTO AgroTrack_Empresa_De_Transportes (Empresa_Id_Empresa) VALUES
+	(9),
+	(10),
+	(11),
+	(12),
+	(13),
+	(14),
+	(15),
+	(16);
+
+INSERT INTO AgroTrack_Contem (Produto_codigo, Quinta_Empresa_Id_Empresa, Quantidade) VALUES
+
+
+INSERT INTO AgroTrack_Colhe(Empresa_Id_Empresa,Agricultor_Pessoa_N_CartaoCidadao, Duracao_colheita,Quantidade,Produto_codigo) VALUES
