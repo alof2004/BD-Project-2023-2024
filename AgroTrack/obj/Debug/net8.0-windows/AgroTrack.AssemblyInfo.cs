d441ef5a326c5b6be907b85fa8699f141b73bb58
alof04 <hpclafonso@gmail.com>
2024-05-20 22:31:03 +0100

    .

--- AgroTrack/obj/Debug/net8.0-windows/AgroTrack.AssemblyInfo.cs
+++ AgroTrack/obj/Debug/net8.0-windows/AgroTrack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgroTrack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+368379696640d66d2d56ce9bbbf0aa63fc825a7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8907ffa50c6ea960aee78f83c1f7a69a37e1ade")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgroTrack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgroTrack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
