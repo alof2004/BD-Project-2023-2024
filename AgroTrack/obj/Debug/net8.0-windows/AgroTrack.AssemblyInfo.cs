66703105c6634be3e1c8cec062c0b14150b126cc
Tomás Brás <tomasbras@ua.pt>
2024-05-23 09:34:23 +0100

    .

--- AgroTrack/obj/Debug/net8.0-windows/AgroTrack.AssemblyInfo.cs
+++ AgroTrack/obj/Debug/net8.0-windows/AgroTrack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgroTrack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8abcaa5e293c57b2d8996a2b62b8672b9b6bfb12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7342d261d82856f4e7b2b75e49794b222f2e8ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgroTrack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgroTrack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
